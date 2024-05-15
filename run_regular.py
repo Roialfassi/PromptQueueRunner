0ea5db86a097676dded026230db2b9a9443bb915
Roialfassi <roialfassi@gmail.com>
2024-05-15 10:16:50 +0300

    added start index
    For keeping progress for multiple runs

--- run_regular.py
+++ run_regular.py
@@ -35,34 +35,36 @@ def query_llm_and_save(prompt, title, credentials_file="credentials.json",
     os.makedirs(output_dir, exist_ok=True)
 
     # Save the response to a .txt file
-    output_file = os.path.join(output_dir, f"{title}.txt")
-    with open(output_file, "w", encoding="utf-8") as f:
+    output_file = os.path.join(output_dir, f"{title}.md")
+    with open(output_file, "w", encoding="utf-8-sig") as f:
         f.write(response)
 
     print(f"Response saved to {output_file}")
 
 
 def load_credentials(credentials_file):
-    with open(credentials_file, "r") as f:
-        credentials = json.load(f)
+    with open(credentials_file, "r", encoding="utf-8-sig") as f:
+        credentials = json.load(f, encoding="utf-8-sig")
     return credentials
 
 
 def process_prompts_from_json(prompts_file, credentials_file="credentials.json",
                               role="I want you to act like a Senior Software Developer"
-                              , model="TheBloke/CodeLlama-7B-Instruct-GGUF/codellama-7b-instruct.Q4_K_S.gguf"):
+                              , model="TheBloke/CodeLlama-7B-Instruct-GGUF/codellama-7b-instruct.Q4_K_S.gguf",
+                              start_index=0):
     with open(prompts_file, "r") as f:
-        prompts_data = json.load(f)
+        prompts_data = json.load(f, encoding="utf-8-sig")
 
-    for prompt_data in prompts_data:
+    for prompt_data in prompts_data[start_index:]:
         prompt = prompt_data["prompt"]
         title = prompt_data["title"]
         query_llm_and_save(prompt, title, credentials_file=credentials_file, role=role, model=model)
 
 
 if __name__ == "__main__":
-    the_model = "TheBloke/CodeLlama-7B-Instruct-GGUF/codellama-7b-instruct.Q4_K_S.gguf"
-    process_prompts_from_json("prompts.json", role="You are a Senior Software Developer specializing in Python,"
-                                                   " each of the prompts are a part of a bigger mission so keep in"
-                                                   " mind to write clean code that is easily integrated",
-                              model=the_model)
+    the_model = "lmstudio-community/Meta-Llama-3-8B-Instruct-GGUF"
+    start_from_index = 170
+    the_role = """
+You are a Hebrew language teacher"""
+    process_prompts_from_json("prompts.json", role=the_role,
+                              model=the_model, start_index= start_from_index)
